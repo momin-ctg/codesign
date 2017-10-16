7005b2526ebe67e60495d8a23a30e9f82495eecf
momin-ctg <mominriyadh@gmail.com>
2017-10-16 15:37:46 +0600

    jquery toggle class

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
     var mainHeader = $('.cd-auto-hide-header'),
         secondaryNavigation = $('.cd-secondary-nav'),
         //this applies only if secondary nav is below intro section
@@ -12,14 +12,14 @@ jQuery(document).ready(function($){
         scrollDelta = 10,
         scrollOffset = 150;
 
-    mainHeader.on('click', '.nav-trigger', function(event){
+    mainHeader.on('click', '.nav-trigger', function (event) {
         // open primary navigation on mobile
         event.preventDefault();
         mainHeader.toggleClass('nav-open');
     });
 
-    $(window).on('scroll', function(){
-        if( !scrolling ) {
+    $(window).on('scroll', function () {
+        if (!scrolling) {
             scrolling = true;
             (!window.requestAnimationFrame)
                 ? setTimeout(autoHideHeader, 250)
@@ -27,7 +27,7 @@ jQuery(document).ready(function($){
         }
     });
 
-    $(window).on('resize', function(){
+    $(window).on('resize', function () {
         headerHeight = mainHeader.height();
     });
 
@@ -47,7 +47,7 @@ jQuery(document).ready(function($){
         if (previousTop - currentTop > scrollDelta) {
             //if scrolling up...
             mainHeader.removeClass('is-hidden');
-        } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+        } else if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
             //if scrolling down...
             mainHeader.addClass('is-hidden');
         }
@@ -57,14 +57,14 @@ jQuery(document).ready(function($){
         //secondary nav below intro section - sticky secondary nav
         var secondaryNavOffsetTop = belowNavHeroContent.offset().top - secondaryNavigation.height() - mainHeader.height();
 
-        if (previousTop >= currentTop ) {
+        if (previousTop >= currentTop) {
             //if scrolling up...
-            if( currentTop < secondaryNavOffsetTop ) {
+            if (currentTop < secondaryNavOffsetTop) {
                 //secondary nav is not fixed
                 mainHeader.removeClass('is-hidden');
                 secondaryNavigation.removeClass('fixed slide-up');
                 belowNavHeroContent.removeClass('secondary-nav-fixed');
-            } else if( previousTop - currentTop > scrollDelta ) {
+            } else if (previousTop - currentTop > scrollDelta) {
                 //secondary nav is fixed
                 mainHeader.removeClass('is-hidden');
                 secondaryNavigation.removeClass('slide-up').addClass('fixed');
@@ -73,12 +73,12 @@ jQuery(document).ready(function($){
 
         } else {
             //if scrolling down...
-            if( currentTop > secondaryNavOffsetTop + scrollOffset ) {
+            if (currentTop > secondaryNavOffsetTop + scrollOffset) {
                 //hide primary nav
                 mainHeader.addClass('is-hidden');
                 secondaryNavigation.addClass('fixed slide-up');
                 belowNavHeroContent.addClass('secondary-nav-fixed');
-            } else if( currentTop > secondaryNavOffsetTop ) {
+            } else if (currentTop > secondaryNavOffsetTop) {
                 //once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset
                 mainHeader.removeClass('is-hidden');
                 secondaryNavigation.addClass('fixed').removeClass('slide-up');
@@ -87,4 +87,6 @@ jQuery(document).ready(function($){
 
         }
     }
+
 });
+
